cd49da74099cb366def50da66bb43422bd63f6c0
Abdelhamedragheb <x2912003x@gmail.com>
2024-04-20 14:32:13 -0400

    update clone_repo

--- clone_repo.sh
+++ clone_repo.sh
@@ -1,4 +1,12 @@
 #!/bin/bash 
 
-read -p "Enter Url of Repo" url
-git clone $url
+read -p "Enter Url of Repo : " url
+# https://github.com/AbdelhamedRagheb/LINUX_SHELL_SCRIPTING
+# check if valid url 
+domainname="github.com"
+if [[ $url == *"$domainname"* ]]
+then
+    git clone $url
+else
+    echo "Please enter valid link"
+fi
